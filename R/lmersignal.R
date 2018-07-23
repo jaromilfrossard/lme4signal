892f1c643c19361c2b140b4ba18f6b0a6006e33e
Jaromil Frossard <jaromil.frossard@gmail.com>
2018-07-23 19:10:46 +0200

    ebauche 1

---
+++ R/lmersignal.R
@@ -0,0 +1,58 @@
+#' Linear Mixed Models with check on the starting value.
+#'
+#' @description This function provide a small modification of the \code{lme4} function \code{link{lmer}} and check if the starting value is a good solution. Works with the JuliaCall and produces faster optimization.
+#' @param formula a \code{lme4} formula.
+#' @param data a data frame. See \code{\link{lmer}} for more details.
+#' @param REML a logical that indicate which criterion to optimize. See \code{\link{lmer}} for more details.
+#' @param control Some parameters. See \link{lmerControl} or \code{\link{lmer}} for more details.
+#' @param start starting values for the parameters. Check if it is a good solution before running the optimizer.
+#' @param verbose See \code{\link{lmer}} for more details.
+#' @param subset an expression to selecte a subset of the data. See \code{\link{lmer}} for more details.
+#' @param weights an optional vector of weights. See \code{\link{lmer}} for more details.
+#' @param na.action a function that handle \code{NA}'s. See \code{\link{lmer}} for more details.
+#' @param offset specify a priori component in the predictor. See \code{\link{lmer}} for more details.
+#' @param contrasts a list of coFntrasts. See \code{\link{lmer}} for more details.
+#' @param devFunOnly a logical set by default to \code{FALSE}. See \code{\link{lmer}} for more details.
+#' @param ... addition arguments. See \code{\link{lmer}} for more details.
+#' @seealso \code{\link{lmer}}.
+#' @importFrom lme4 lmerControl glmerControl mkLmerDevfun optimizeLmer checkConv mkMerMod
+#' @export
+lmersignal <- function (formula, data = NULL, REML = TRUE, control = lmerControl(),
+          start = NULL, verbose = 0L, subset, weights, na.action, offset,
+          contrasts = NULL, devFunOnly = FALSE, ...) {
+  mc <- mcout <- match.call()
+  missCtrl <- missing(control)
+  if (!missCtrl && !inherits(control, "lmerControl")) {
+    if (!is.list(control))
+      stop("'control' is not a list; use lmerControl()")
+    warning("passing control as list is deprecated: please use lmerControl() instead",
+            immediate. = TRUE)
+    control <- do.call(lmerControl, control)
+  }
+  if (!is.null(list(...)[["family"]])) {
+    warning("calling lmer with 'family' is deprecated; please use glmer() instead")
+    mc[[1]] <- quote(lme4::glmer)
+    if (missCtrl)
+      mc$control <- glmerControl()
+    return(eval(mc, parent.frame(1L)))
+  }
+  fr = model.frame(nobars(formula),df)
+  signal = model.response(fr)
+
+  #signal <- model.response(lmod$fr)
+
+  mc$control <- control
+  mc[[1]] <- quote(lme4::lmer)
+  mc$formula = update.formula(old= formula(mc),new = eval(parse(text=paste(paste(nobars(formula)[[2]]),"[,1]~.",sep=""))))
+  print(dim(signal))
+  output=list()
+  output[[1]] <- eval(mc)
+  for(i in 2:ncol(signal)){
+    newy = paste(paste(nobars(formula)[[2]]),"[,",eval(parse(text="i")),"]~.",sep="")
+    mc$formula = update.formula(old= formula(mc),new = eval(parse(text=newy)))
+    mc$start= getME(output[[i-1]],"theta")
+    output[[i]] = eval(mc)
+    print(i)
+  }
+  return(output)
+}
---
+++ test.R
@@ -0,0 +1,72 @@
+rm(list=ls())
+library(lme4)
+lf= list.files("R")
+for(i in lf){print(i);source(paste("R/",i,sep=""))}
+
+load("data_18i_20s.RData")
+
+
+
+
+df=data_signal$design
+signal20=data_signal$y
+
+df$Anum = model.matrix(~A,df,contrasts.arg = list(A=contr.poly))[,-1]
+df = cbind(df,as.matrix(model.matrix(~B,df,contrasts.arg = list(B=contr.poly))[,-1]))
+df$Cnum = model.matrix(~C,df,contrasts.arg = list(C=contr.poly))[,-1]
+
+colnames(df)[7:8] =c("Bnum1","Bnum2")
+contrasts(df$A)= contr.poly
+contrasts(df$B)= contr.poly
+contrasts(df$C)= contr.poly
+
+
+
+
+#flmer = y~A*B*C + Error(id/(B*C))+ Error(item/(A*C))
+
+flmer = y~A*B*C + ((Bnum1+Bnum2)*Cnum||id)+ ((Anum*Cnum)||item)
+f_signal = signal20~A*B*C + ((Bnum1+Bnum2)*Cnum||id)+ ((Anum*Cnum)||item)
+
+m_signal = lmersignal(f_signal,df,REML = T)
+
+
+ranefmat= sapply(m_signal,function(model)ranef(model)$item[,1])
+ts.plot(t(ranefmat))
+
+ranefmat= sapply(m_signal,function(model)ranef(model)$id[,1])
+ts.plot(t(ranefmat))
+
+
+thetas = sapply(m_signal,function(model)getME(model,"theta"))
+ts.plot(t( thetas))
+
+mc$formula
+
+
+lme4:::formula.
+
+lme4:::getResponseFormula()
+model.response(f_signal)
+model.response(lmod$fr)
+
+findbars(f_signal)
+nobars(f_signal)
+nlformula(f_signal)
+
+df$y = data_signal$y[,1]
+m_bobyqa= lmer(flmer,df,REML = T)
+
+getME(m_bobyqa,"theta")
+
+t0=proc.time()
+m_bobyqa2 <- update(m_bobyqa, control = lmerControl(optimizer="bobyqa"))
+proc.time()-t0
+
+cbind(getME(m_bobyqa,"theta"),getME(m_bobyqa2,"theta"))
+
+
+
+t0=proc.time()
+m_nm <- update(m_bobyqa, control = lmerControl(optimizer="Nelder_Mead"))
+proc.time()-t0
